d38d55fe2b64b0291254506c00d520d8e2fb025b
Owesomewilser <owesomewilser@gmail.com>
2025-04-06 01:38:48 -0500

    Added basecamp scene, shifted player light, tried to add pickups
    (failed to add pickups)

---
+++ scenes/basecamp.gd
@@ -0,0 +1,7 @@
+extends Node
+
+
+func _on_launch_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+#Hey Austin, I copied your Title screen and injected this. I'll update the art.
---
+++ scenes/component/pickup_magnet.gd
@@ -0,0 +1,16 @@
+class_name pickup_magnet extends Area2D
+
+
+var pickups : Array[resource_pickup] = []
+var speeds : Array[float] = []
+
+@export var magnet_strength : float = 1.0
+
+func _ready() -> void:
+	area_entered.connect( _on_area_enter )
+	pass
+
+func _on_area_enter( _a : Area2d ) -> void:
+	if _a.get_parent()
+
+#this shit ain't done. Was meant to serve as the player's radius for vacuuming "repair" and "data" pickups
