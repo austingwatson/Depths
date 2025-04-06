debd3d171cb4e0c2c966e2a64bb1be41ea5f1aad
Austin Watson <austingwatson@hotmail.com>
2025-04-06 00:01:04 -0500

    fixed small sonar always active

--- scripts/component/sonar.gd
+++ scripts/component/sonar.gd
@@ -3,13 +3,16 @@ extends Area2D
 
 @export var max_radius := 0.0
 @export var duration := 0.0
-@onready var collision_shape: Shape2D = $CollisionShape2D.shape
+@onready var collision_shape := $CollisionShape2D
+@onready var shape: Shape2D = collision_shape.shape
 
 
 func start_sonar() -> void:
+	collision_shape.set_deferred("disabled", false)
 	var tween := create_tween()
-	await tween.tween_property(collision_shape, "radius", max_radius, duration).finished
-	collision_shape.radius = 0.0
+	await tween.tween_property(shape, "radius", max_radius, duration).finished
+	shape.radius = 0.0
+	collision_shape.set_deferred("disabled", true)
 
 
 func _on_area_entered(area: Area2D) -> void:
