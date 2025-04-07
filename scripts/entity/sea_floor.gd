e209fcdf58a3b9f4c9234ec8c4940c580e198a2f
Austin Watson <austingwatson@hotmail.com>
2025-04-07 17:19:59 -0500

    small change

--- scripts/entity/sea_floor.gd
+++ scripts/entity/sea_floor.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	global_position.x = get_viewport().get_camera_2d().global_position.x
 
 
