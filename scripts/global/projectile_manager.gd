9c8407935ee9ce39195cabb6492a201cd0a389f9
Austin Watson <austingwatson@hotmail.com>
2025-04-07 16:10:29 -0500

    added weapon switching icon

--- scripts/global/projectile_manager.gd
+++ scripts/global/projectile_manager.gd
@@ -2,6 +2,7 @@ extends Node
 
 const torpedo_scene := preload("res://scenes/entity/projectile/torpedo.tscn")
 const arrow_scene := preload("res://scenes/entity/projectile/arrow.tscn")
+const weapon_icon_scene := preload("res://scenes/entity/projectile/weapon_icon.tscn")
 @export_flags_2d_physics var friendly_layer: int
 @export_flags_2d_physics var friendly_mask: int
 
@@ -18,3 +19,10 @@ func add_arrow(global_position: Vector2, target_position: Vector2) -> void:
 	arrow.look_at(target_position)
 	arrow.rotation_degrees -= 90
 	arrow.global_position += arrow.global_position.direction_to(target_position) * 100.0
+	
+
+func add_weapon_icon(global_position: Vector2, weapon: int) -> void:
+	var weapon_icon := weapon_icon_scene.instantiate()
+	add_child(weapon_icon)
+	weapon_icon.global_position = global_position + (Vector2.UP * 32)
+	weapon_icon.init(weapon)
