f095d2a58f613454a7dce4027ef527ebd6d05037
Owesomewilser <owesomewilser@gmail.com>
2025-04-07 14:44:57 -0500

    Added titlecard, intro scene, made more fish spawn, added bad particles

--- scripts/component/research_beam.gd
+++ scripts/component/research_beam.gd
@@ -5,11 +5,15 @@ extends Area2D
 func _on_area_entered(area: Area2D) -> void:
 	if area is ResourcePickup:
 		area.start_scan()
+	#get_node("ScanBeam").self_modulate = Color8(#fbae24) "This is supposed to make the scna beam appear orange"
 
 
 func _on_area_exited(area: Area2D) -> void:
 	if area is ResourcePickup:
 		area.end_scan()
+	#get_node("ScanBeam").self_modulate = Color8(#fbae2400) "This is supposed to make it become invisible again"
+
+
 
 
 func _on_scan_timer_timeout() -> void:
