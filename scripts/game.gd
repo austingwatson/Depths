0d95eb379665feef20fce426c700714f0a3a8f17
Austin Watson <austingwatson@hotmail.com>
2025-04-07 17:34:36 -0500

    added fish anim

--- scripts/game.gd
+++ scripts/game.gd
@@ -15,21 +15,21 @@ func _ready() -> void:
 	await get_tree().create_timer(0.5).timeout
 	player.initial_drop = false
 	
-	var urchin := preload("res://scenes/entity/actor/urchin.tscn").instantiate()
-	urchin.global_position = Vector2(50, 50)
-	EntityManager.add_entity(urchin, true)
+	#var urchin := preload("res://scenes/entity/actor/urchin.tscn").instantiate()
+	#urchin.global_position = Vector2(50, 50)
+	#EntityManager.add_entity(urchin, true)
 	
-	var floater := preload("res://scenes/entity/actor/floater.tscn").instantiate()
-	floater.global_position = Vector2(100, 100)
-	EntityManager.add_entity(floater, true)
+	#var floater := preload("res://scenes/entity/actor/floater.tscn").instantiate()
+	#floater.global_position = Vector2(100, 100)
+	#EntityManager.add_entity(floater, true)
 	
 	#var hunter_fish := preload("res://scenes/entity/actor/hunter_fish.tscn").instantiate()
 	#hunter_fish.global_position = Vector2(-100, -100)
 	#EntityManager.add_entity(hunter_fish, true)
 	
-	var research_pickup := preload("res://scenes/entity/drops/resource_pickup.tscn").instantiate()
-	research_pickup.global_position = Vector2(-50, 100)
-	EntityManager.add_entity(research_pickup, true)
+	#var research_pickup := preload("res://scenes/entity/drops/resource_pickup.tscn").instantiate()
+	#research_pickup.global_position = Vector2(-50, 100)
+	#EntityManager.add_entity(research_pickup, true)
 	
 	GlobalSignals.player_moved.connect(on_player_moved)
 	
