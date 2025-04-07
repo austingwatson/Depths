0b79a3e7aed188c40b17389dc6ccca551a58daaf
Owesomewilser <owesomewilser@gmail.com>
2025-04-07 15:34:12 -0500

    Added intro screen and placeholder tutorial page

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -1,1 +1,11 @@
 extends Node
+
+func _ready() -> void:
+	$TextScroller.play("text_scroll")
+
+func _on_tutorial_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
+
+
+func _on_skip_tutorial_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/basecamp.tscn")
