79d40773be84c37c7f8bc727fbf7d652ea33fe41
Austin Watson <austingwatson@hotmail.com>
2025-04-16 15:15:31 -0500

    added scuttle option

---
+++ scripts/ui/scuttle.gd
@@ -0,0 +1,30 @@
+extends TextureProgressBar
+
+@onready var timer := $Timer
+var hovered := false
+var can_press := false
+
+
+func _physics_process(_delta: float) -> void:
+	if hovered:
+		value = timer.wait_time - timer.time_left
+	
+	if can_press and Input.is_action_just_pressed("shoot"):
+		EntityManager.clear()
+		get_tree().change_scene_to_file("res://scenes/basecamp.tscn")
+
+
+func _on_mouse_entered() -> void:
+	timer.start()
+	hovered = true
+
+
+func _on_mouse_exited() -> void:
+	timer.stop()
+	hovered = false
+	can_press = false
+	value = 0
+
+
+func _on_timer_timeout() -> void:
+	can_press = true
